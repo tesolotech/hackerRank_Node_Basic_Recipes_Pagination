c4b84c38c3d435ba96edf7bd6b138ecc8853fed1
vikash <vikash.kumar@appzlogic.com>
2022-01-24 12:29:06 +0530

    fix detailt issues

--- controllers/recipts.js
+++ controllers/recipts.js
@@ -1,32 +1,36 @@
 const rec = require('../recipes.json');
 
 exports.getReceipts = (req, res) => {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 3;
-    // calculating the starting and ending index
-    const startIndex = (page - 1) * limit;
-    const endIndex = page * limit;
+    if (req.query.page && req.query.limit) {
+        const page = parseInt(req.query.page);
+        const limit = parseInt(req.query.limit);
+        // calculating the starting and ending index
+        const startIndex = (page - 1) * limit;
+        const endIndex = page * limit;
 
-    const results = {};
-    if (endIndex < rec.length) {
-        results.next = {
-            page: page + 1,
-            limit: limit
-        };
-    }
-    if (startIndex > 0) {
-        results.previous = {
-            page: page - 1,
-            limit: limit
-        };
+        const results = {};
+        if (endIndex < rec.length) {
+            results.next = {
+                page: page + 1,
+                limit: limit
+            };
+        }
+        if (startIndex > 0) {
+            results.previous = {
+                page: page - 1,
+                limit: limit
+            };
+        }
+        let response = {
+            status: 200,
+            message: "SUCEESS",
+            data: rec.slice(startIndex, endIndex)
+        }
+        res.status(200).json(response);
+    } else {
+        res.status(200).json(rec);
     }
 
-    let response = {
-        status: 200,
-        message: "SUCEESS",
-        data: rec.slice(startIndex, endIndex)
-    }
-    res.status(200).json(response);
 
 }
 
