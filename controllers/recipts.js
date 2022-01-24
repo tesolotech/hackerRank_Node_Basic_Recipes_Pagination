fd91681facf2a380f51cc0d43227323cf6104e1b
vikash <vikash.kumar@appzlogic.com>
2022-01-24 12:15:47 +0530

    added pagination methods

---
+++ controllers/recipts.js
@@ -0,0 +1,32 @@
+const rec = require('../recipes.json');
+
+exports.getReceipts = (req, res) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
+    // calculating the starting and ending index
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+
+    const results = {};
+    if (endIndex < rec.length) {
+        results.next = {
+            page: page + 1,
+            limit: limit
+        };
+    }
+    if (startIndex > 0) {
+        results.previous = {
+            page: page - 1,
+            limit: limit
+        };
+    }
+
+    let response = {
+        status: 200,
+        message: "SUCEESS",
+        data: rec.slice(startIndex, endIndex)
+    }
+    res.status(200).json(response);
+
+}
+
--- routes/recipes.js
+++ routes/recipes.js
@@ -1,5 +1,8 @@
 var recipes = require('../recipes.json');
 var router = require('express').Router();
+const recipts = require('../controllers/recipts.js')
+
+router.get('/', recipts.getReceipts);
 
 module.exports = router;
 
